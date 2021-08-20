80e5d2d924bd981cdb804a47817128d561d50bf3
mayke.furtado <mayke.furtado@arcom.com.br>
2021-08-20 13:46:42 -0300

    init

---
+++ src/view/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import NovoUsuario from "./NovoUsuario";
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <NovoUsuario/>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ src/view/NovoUsuario/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const NovoUsuario = () => (
+    <View>
+      <Text>Ola Mundo Mayke</Text>
+    </View>
+);
+
+
+export default NovoUsuario;
